b5ec9319e95086b693145cc082343599bb167817
Ryan Smith <rsmith@cgull.local>
2009-08-10 12:52:14 -0700

    created a dock for the web apps

--- public/application.js
+++ public/application.js
@@ -1,6 +1,5 @@
 $(document).ready( function() {
     $('#web_apps').bind( 'click', function(){
-        $(this).css('border-bottom','1px solid red')
         $('#menu').slideToggle();
     });    
 });
